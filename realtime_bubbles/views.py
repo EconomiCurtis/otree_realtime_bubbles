0fb6284aca9c9a7f0a8b565f17f38b5043b6a6de
Curtis Kephart <curtisesjunk@gmail.com>
2017-11-02 09:15:24 +0400

    output data
    - add to views.py output data

--- realtime_bubbles/views.py
+++ realtime_bubbles/views.py
@@ -3,14 +3,16 @@ from . import models
 from ._builtin import Page, WaitPage
 from .models import Constants
 import random as rand
+import json
+from django.conf import settings
 
 class MyPage(Page):
     
     def vars_for_template(self):
 
     	return{
-    		'rand_init_val':rand.random()
-
+    		'rand_init_val':rand.random(),
+            'DEBUG':settings.DEBUG
 
     	}
 
@@ -23,6 +25,54 @@ class ResultsWaitPage(WaitPage):
 
 class Results(Page):
     pass
+
+
+def get_output_table(events):
+
+    
+    header = [
+        'timestamp',
+        'session_code',
+        'player_code',
+        'round_number',
+        'group_id',
+        'tick',
+        'x',
+    ]
+    if not events:
+        return [], []
+    rows = []
+ 
+    group = events[0].group
+
+    tick = 0
+    participant_dict = {}
+    for event in events:
+        if event.channel == 'group_decisions':
+
+            # if participant_dict[event.participant] == None:
+            #     participant_dict[event.participant] = []
+            
+            # participant_dict[event.participant].push(event.value)
+
+
+
+
+
+
+            rows.append([
+                event.timestamp,
+                group.session.code,
+                event.participant,
+                group.round_number  ,
+                group.id_in_subsession,
+                tick,
+                event.value,
+            ])
+            tick += 1
+
+
+    return header, rows
 
 
 page_sequence = [
